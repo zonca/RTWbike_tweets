7f82bc1bd185685e6dfe84f0a62fa15f452741cc
Andrea Zonca <code@andreazonca.com>
2017-10-27 02:54:29 -0700

    update script

---
+++ update.sh
@@ -0,0 +1,5 @@
+cd /home/zonca/zonca/p/software/tweet_dumper
+/home/zonca/anaconda/envs/tweet_dumper/bin/python /home/zonca/zonca/p/software/tweet_dumper/tweet_image_dumper.py
+git add RTWbike_tweets.csv
+git commit -m "Update $(date)"
+git push
